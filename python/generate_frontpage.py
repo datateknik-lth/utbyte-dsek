5a0e3dfa363b4401a5178c2b72c7c0e222103976
Sebastian Hegardt <se.hegardt@gmail.com>
2018-11-25 14:21:42 +0100

    Db changes

--- python/generate_frontpage.py
+++ python/generate_frontpage.py
@@ -1,11 +1,22 @@
+import os
+
 def main():
     markdown = "# Utbyte på Datatekniksektionen vid TLTH\nVälkomna! Här hittar ni nyttiga resurser för ert utbyte. " \
                "Vi samlar här främst nyttiga länkar och studieplaner, men även *Statement of Purpose*."
 
-    table = "Land|Universitet|"
+    allrows = ""
+    for country in os.listdir("../db"):
+        row = html_row(country.capitalize(), "")
+        allrows += row + "\n"
 
-    with open("README.md", 'w+', encoding="'utf-16") as file:
+    table = "<table>\n<tbody>\n<tr>\n<th>Land</th>\n<th>Universitet</th></tr>\n{}\n</tbody>\n</table>".format(allrows)
+
+    with open("../index.md", 'w+', encoding="'utf-16") as file:
         file.write(markdown + '\n' + table)
+
+
+def html_row(country, uni):
+    return "<tr><td>{}</td><td>{}</td></tr>".format(country, uni)
 
 if __name__ == "__main__":
     main()
--- python/uni_markdown.py
+++ python/uni_markdown.py
@@ -1,5 +1,10 @@
 class Uni:
 
     def __init__(self, name):
-        self.markdown = "#{}\n##Studieplaner\n##Länkar\nHär finner ni nyttiga länkar till universitetet, t.ex. till " \
-                        "kurser eller information speciellt till utbytesstudenter.".format(name)
+        self.table_base = "[1]: Namnet på den vars studieplan det är. [2] Reflekterar studieplanen det jag faktiskt läste " \
+                          "i landet eller är detta endast det jag tänkte lästa?\n\n|Länk|Namn [1]|Godkänd av programledning?|Det jag faktiskt läste i landet? [2]|\n" \
+                          "|----|-----|-----|-----|\n|||||"
+        self.markdown = "#{}\n##Studieplaner\n{}\n##Länkar\nHär finner ni nyttiga länkar till universitetet, t.ex. till " \
+                        "kurser eller information speciellt till utbytesstudenter.".format(name, self.table_base)
+
+

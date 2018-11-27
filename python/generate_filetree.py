e9ca5a7ee7803c1ded51183f9f11aa3f55410ca0
Sebastian Hegardt <se.hegardt@gmail.com>
2018-11-27 01:01:30 +0100

    Updated filestructure and uni page

--- python/generate_filetree.py
+++ python/generate_filetree.py
@@ -3,13 +3,26 @@ import json
 import os
 import re
 import unicodedata
-from uni_markdown import Uni
 
 
 def standardize(name):
     name = unicodedata.normalize('NFKD', name).encode('ascii', 'ignore').decode('ascii')
     name = re.sub(' ', '_', name).lower().strip()
     return name
+
+
+def create_html(uni_name, uni_path):
+    # Create HTML base
+    with open('../template/uni_template.html', 'r', encoding='utf-8') as file:
+        html = file.read()
+
+    html = html.replace("#title", uni_name)
+    html = html.replace("#header", uni_name)
+
+    with open("{}/{}.html".format(uni_path, standardize(uni_name)), 'w+', encoding='utf-8') as file:
+        file.write(html)
+
+CALI_UNIS = ["UC Berkeley", "UC Davis", "UC Irvine", "UCLA", "UC Merced", "UC Riverside", "UC San Diego", "UC San Francisco", "UC Santa Barbara", "UC Santa Cruz"]
 
 
 @click.command()
@@ -21,25 +34,25 @@ def main(file):
     for country in data:
         unis = data[country]
         path = "../db/{}".format(country)
-        if not os.path.exists(path):
-            os.makedirs('../db/{}'.format(standardize(country)))
-            print("{}".format(country))
+        print("{}".format(country))
 
         for uni in unis:
+
             uni_path = path + "/{}".format(standardize(uni))
             if not os.path.exists(uni_path):
                 os.makedirs(uni_path)
                 print("  |-->{}".format(uni))
 
-            # Create HTML base
-            with open('../template/uni_template.html', 'r', encoding='utf-8') as file:
-                html = file.read()
+            if country == "usa" and "university_of_california" == standardize(uni):
+                for cali_uni in CALI_UNIS:
+                    uni_cali_path = uni_path + "/{}".format(standardize(cali_uni))
+                    if not os.path.exists(uni_cali_path):
+                        os.makedirs(uni_cali_path)
+                        print("     |-->{}".format(cali_uni))
+                        create_html("{} - {}".format(uni, cali_uni), uni_cali_path)
+            else:
+                create_html(uni, uni_path)
 
-            html = html.replace("#title", uni)
-            html = html.replace("#header", uni)
-
-            with open("{}/{}.html".format(uni_path, standardize(uni)), 'w+', encoding='utf-8') as file:
-                file.write(html)
 
 
 if __name__ == "__main__":

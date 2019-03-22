6bcb7c8907fd6595d16a4935de4a121b6971f81e
Sebastian Hegardt <se.hegardt@gmail.com>
2019-03-22 18:00:40 +0100

    Added constants for host and port

--- src/misc/HTTPRequester.js
+++ src/misc/HTTPRequester.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
+const HOST = "http://localhost"
+const PORT = 4000;
+
 export function postStudyPlan(plan, headers) {
-    return fetch("http://localhost:4000/study-plans/add", {
-        method: "POST",
-        body: plan,
-        headers: headers
-    })
+    debugger;
+    console.log(plan);
+    return fetch(`${HOST}:${PORT}/study-plans/add`, {
+            method: "POST",
+            body: plan,
+            headers: headers})
         .then(res => res.text())
     // return axios.post("http://localhost:4000/study-plans/add", plan, {
     //     headers: {
